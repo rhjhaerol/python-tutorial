fe331b12dabf0d61acc426b240ae4297a823898c
rhjhaerol <rhjhaerol@gmail.com>
2024-03-13 11:50:02 +0700

    add main

---
+++ mini-market/main.py
@@ -0,0 +1,19 @@
+from libs import welcome_message, exit_program
+from tools import warung
+
+def menu():
+    user_option = int(input("Menu Program:\n1. Mini Market\n2. Exit\n\nPlease select: "))
+    
+    if user_option == 1:
+        warung.start()
+    elif user_option == 2:
+        exit_program()
+    else:
+        print('You can only choose what is available on the menu!')
+
+def main():
+    welcome_message()
+    menu()
+    
+if __name__ == '__main__':
+    main()
